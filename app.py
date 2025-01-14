fd97cf07ecd36ee89809cada88f35887163c5ac3
sanatwalia896 <121603801+sanatwalia896@users.noreply.github.com>
2025-01-14 13:08:44 +0530

    Completed SQL chatbot

--- app.py
+++ app.py
@@ -79,3 +79,35 @@ else:
     st.success("Connected to the SQLite database!")
 
 # Further implementation with `toolkit` and SQL agent.
+
+toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+
+agent = create_sql_agent(
+    llm=llm,
+    toolkit=toolkit,
+    verbose=True,
+    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+)
+
+if "messages" not in st.session_state or st.sidebar.button("Clear Message history"):
+    st.session_state["messages"] = [
+        {"role": "assistant", "content": "How can I help you? "}
+    ]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+
+user_query = st.chat_input(placeholder="Ask anything from the database")
+
+if user_query:
+    st.session_state.messages.append({"role": "user", "content": user_query})
+
+    st.chat_message("user").write(user_query)
+
+    with st.chat_message("assistant"):
+        streamlit_callback = StreamlitCallbackHandler(st.container())
+        response = agent.run(user_query, callbacks=[streamlit_callback])
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+        st.write(response)
